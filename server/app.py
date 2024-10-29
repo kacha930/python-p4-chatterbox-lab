f0e463079aecf574cd3ee78b4da1720dc03e50dc
kacha930 <robiwere20@gmail.com>
2024-10-29 19:33:59 +0300

    success

--- server/app.py
+++ server/app.py
@@ -14,13 +14,56 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages' , methods=['GET'])
 def messages():
-    return ''
+    messages = Message.query.order_by(Message.created_at.asc()).all()
+    messages_list = [message.to_dict() for message in messages]
+    return make_response(jsonify(messages_list), 200)
 
-@app.route('/messages/<int:id>')
+
+@app.route('/messages/<int:id>' , methods=['GET'])       
 def messages_by_id(id):
-    return ''
+    message = Message.query.filter_by(id=id).first()
+    if not message:
+        return jsonify({'error': 'Message not found'}), 404
+    response = make_response(jsonify(message.t0_dict()))
+    return response
+@app.route('/messages', methods=['POST'])
+def create_message():
+    data = request.get_json()
+    body = data.get('body')
+    username = data.get('username')
+    if not body or not username:
+        return jsonify({'error': 'Missing required fields'}), 400
+    message = Message(body=body, username=username)
+    db.session.add(message)
+    db.session.commit()
+    return jsonify(message.to_dict()), 201
+
+@app.route('/messages/<int:id>', methods=['PATCH'])
+def update_message(id):
+    message = Message.query.filter_by(id=id).first()
+    if not message:
+        return jsonify({'error': 'Message not found'}), 404
+    data = request.get_json()
+    body = data.get('body')
+    if body:
+        message.body = body
+    db.session.commit()
+    return jsonify(message.to_dict()), 200
+
+@app.route('/messages/<int:id>', methods=['DELETE'])
+def delete_message(id):
+    message = Message.query.filter_by(id=id).first()
+    if not message:
+        return jsonify({'error': 'Message not found'}), 404
+    db.session.delete(message)
+    db.session.commit()
+    return jsonify({'message': 'Message deleted'}), 204
+
+
+
+
 
 if __name__ == '__main__':
     app.run(port=5555)
